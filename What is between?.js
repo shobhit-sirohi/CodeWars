e23043d64a5edcf2074fed535cf07ff71a39ec42
Shobhit Sirohi <shobhitsirohi.dev@gmail.com>
2022-05-16 02:13:07 +0530

    what is bwtween

--- What is between?.js
+++ What is between?.js
@@ -8,3 +8,24 @@
 
 // 8 kyu
 
+function between(a, b) {
+  let arr = [];
+  let i = 1;
+  while (a <= b) {
+    arr.splice(i, 0, a++);
+    i++;
+    a = a++;
+  }
+  return arr;
+}
+
+
+//BETTER SOLUTION
+
+function between(a, b) {
+    arr = []
+    for(i = a;i <= b; i++){ 
+        arr.push(i)
+    }
+    return arr
+  }
